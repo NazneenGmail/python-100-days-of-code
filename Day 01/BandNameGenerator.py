ff1c5db0b5ab07ca352c309b67decd40dc679711
Nazneen Dadhiwale <nazneendadhiwale@Nazneens-MacBook-Pro.local>
2024-09-26 14:11:20 -0700

    First commit - Day 01

---
+++ Day 01/BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city = input("In which city were you born?\n")
+pet = input("What was your first pet name?\n")
+print("Your band name could be " + city + " " + pet)

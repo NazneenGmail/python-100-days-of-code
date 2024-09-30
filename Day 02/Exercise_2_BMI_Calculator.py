03597b06d45d2b9a14f7d5b818ee70fcc07a7d78
Nazneen Dadhiwale <nazneendadhiwale@Nazneens-MacBook-Pro.local>
2024-09-30 15:36:21 -0700

    Updated Day 02 files

---
+++ Day 02/Exercise_2_BMI_Calculator.py
@@ -0,0 +1,12 @@
+height = 1.65
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = weight / (height * height)
+
+print(f"Your BMI is {int(bmi)}")
+
+print(f"Your BMI is rounded to {round(bmi)}")
+
+print(f"Your BMI is rounded to 2 digits {round(bmi, 2)}")
